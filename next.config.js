cd11ee76ef76fc8e81d63037b55932005e1cb4dd
Miguelo981 <lorito20@hotmail.es>
2022-08-09 15:31:19 +0200

    - Next config added

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    async headers() {
+      return [
+        {
+          source: "/",
+          headers: [
+            {
+              key: "Cache-Control",
+              value: "s-maxage=1, stale-while-revalidate=59",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -153,14 +153,3 @@ const IndexPage = () => {
 }
 
 export default IndexPage
-
-export async function getServerSideProps({ req, res }) {
-  res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=10, stale-while-revalidate=59'
-  )
-
-  return {
-    props: {},
-  }
-}
